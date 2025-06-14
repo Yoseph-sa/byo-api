7b44c9c94d65241c8036ac5e0e617aeef508ac1b
erraghavchhabra <howtodonethis@gmial.com>
2025-06-14 14:22:17 +0530

    update code

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,11 +1,21 @@
-import about from "./about";
-import category from "./category";
-import client from "./client";
-import contact from "./contact";
-import faq from "./faq";
-import gallery from "./gallery";
-import home from "./home";
-import projects from "./projects";
-import services from "./services";
+import about from './about'
+import category from './category'
+import client from './client'
+import contact from './contact'
+import faq from './faq'
+import gallery from './gallery'
+import home from './home'
+import projects from './projects'
+import services from './services'
 
-export const schemaTypes = [home,category,projects,client , services , about , contact , faq , gallery]
+export const schemaTypes = [
+  home,
+  category,
+  projects,
+  client,
+  services,
+  about,
+  contact,
+  faq,
+  gallery,
+]
